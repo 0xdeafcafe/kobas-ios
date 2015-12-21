7a55c740bbc5dc71ab9d7feb9b07933db23a1470
Alex Forbes-Reed <alexreed@outlook.com>
2015-12-21 05:08:53 +0000

    added primitive profile picture displaying

--- kobas-ios/HomeViewController.swift
+++ kobas-ios/HomeViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class HomeViewController: UIViewController {
 	
 	@IBOutlet weak var WelcomeTitleLabel: UILabel!
+	@IBOutlet weak var ProfilePictureImageView: UIImageView!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 		
-		WelcomeTitleLabel.text = AppDelegate.kobasApiClient.getAuthenticatedStaff()?.name
-    }
+		WelcomeTitleLabel.text = String(format: "Welcome to Kobas\n%@!", (AppDelegate.kobasApiClient.getAuthenticatedStaff()?.name)!)
+		
+		let url = NSURL(string: String(format: "https://kobas.s3.amazonaws.com/%@", AppDelegate.kobasApiClient.getAuthenticatedStaff()!.profileImageUrl!))
+		let data = NSData(contentsOfURL: url!)
+		ProfilePictureImageView.image = UIImage(data: data!)
+	}
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
